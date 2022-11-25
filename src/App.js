d438aef14f2a07cdf0089ec8817b279e84dea443
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-11-25 10:30:00 +0700

    10.1.2-validate-form-react

--- src/App.js
+++ src/App.js
@@ -3,29 +3,46 @@ import Nav from "./components/Navigation/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        {/* <Nav /> */}
-        <Switch>
-          <Route path="/news">news</Route>
-          <Route path="/about">about</Route>
-          <Route path="/contact">contact</Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/" exact>
-            home
-          </Route>
-          <Route path="*">404 not found</Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="app-container">
+          {/* <Nav /> */}
+          <Switch>
+            <Route path="/news">news</Route>
+            <Route path="/about">about</Route>
+            <Route path="/contact">contact</Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/" exact>
+              home
+            </Route>
+            <Route path="*">404 not found</Route>
+          </Switch>
+        </div>
+      </Router>
+
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,6 +2,7 @@ import "./Register.scss";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 const Register = (props) => {
   const [email, setEmail] = useState("");
@@ -21,7 +22,37 @@ const Register = (props) => {
     // });
   }, []);
 
+  const isValidInputs = () => {
+    if (!email) {
+      toast.error("Email is required!");
+      return false;
+    }
+    if (!phone) {
+      toast.error("Phone is required!");
+      return false;
+    }
+    if (!password) {
+      toast.error("Password is required!");
+      return false;
+    }
+
+    if (password != confirmPassword) {
+      toast.error("Your password is not the same");
+      return false;
+    }
+
+    let regx = /\S+@\S+\.\S+/;
+    if (!regx.test(email)) {
+      toast.error("Please enter a valid email address");
+      return false;
+    }
+
+    return true;
+  };
+
   const handleRegister = () => {
+    let check = isValidInputs();
+
     let userData = { email, phone, username, password };
 
     console.log(">>> check userData: ", userData);
@@ -93,6 +124,7 @@ const Register = (props) => {
 
             <button
               className="btn btn-primary"
+              type="button"
               onClick={() => handleRegister()}
             >
               Register
