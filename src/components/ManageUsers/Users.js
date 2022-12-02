30da116b79d481980571222a5056ccb31b22e04f
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-12-02 13:26:55 +0700

    10.6.1-display-list-users

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,7 +1,102 @@
+import { useEffect, useState } from "react";
 import "./Users.scss";
+import { fetchAllUsers } from "../../services/userService";
 
 const Users = (props) => {
-  return <div>users components</div>;
+  const [listUsers, setListUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    let response = await fetchAllUsers();
+    if (response && response.data && response.data.EC === 0) {
+      setListUsers(response.data.DT);
+      console.log(response.data.DT);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="manage-users-container">
+        <div className="user-header">
+          <div className="title">
+            <h3>Table Users</h3>
+          </div>
+          <div className="actions">
+            <button className="btn btn-success">Refresh</button>
+            <button className="btn btn-primary">Add new user</button>
+          </div>
+        </div>
+        <div className="user-body">
+          <table class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">ID</th>
+                <th scope="col">Email</th>
+                <th scope="col">Username</th>
+                <th scope="col">Group</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listUsers && listUsers.length > 0 ? (
+                <>
+                  {listUsers.map((item, index) => {
+                    return (
+                      <tr key={`row-${index}`}>
+                        <td>{index + 1}</td>
+                        <td>{item.id}</td>
+                        <td>{item.email}</td>
+                        <td>{item.username}</td>
+                        <td>{item.Group ? item.Group.name : ""}</td>
+                      </tr>
+                    );
+                  })}
+                </>
+              ) : (
+                <>
+                  <span>Not found users</span>
+                </>
+              )}
+            </tbody>
+          </table>
+        </div>
+        <div className="user-footer">
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  Previous
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  3
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
