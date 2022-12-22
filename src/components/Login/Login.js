196a17d637a707932ccfb2199e85adfe8c571cf3
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-12-22 16:59:53 +0700

    11.11-bearer-authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -51,6 +51,7 @@ const Login = (props) => {
         account: { groupWithRoles, email, username },
       };
 
+      localStorage.setItem("jwt", token);
       loginContext(data);
       history.push("/users");
     }
